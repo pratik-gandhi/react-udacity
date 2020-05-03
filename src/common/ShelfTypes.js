446fba5ebc22b1950a904c7e1aa7762234cc40d1
pratik-gandhi <pratik.gandhi@outlook.com>
2020-05-03 15:46:48 +0200

    Add book categories

---
+++ src/common/ShelfTypes.js
@@ -0,0 +1,8 @@
+const shelfTypes = {
+  "CurrentlyReading": { name: "Currently Reading", value: "currentlyReading" },
+  "WantToRead": { name: "Want to Read", value: "wantToRead" },
+  "Read": { name: "Read", value: "read" },
+  "None": { name : "None", value: "none"}
+};
+
+export default shelfTypes
